d8c8b21e1ca5e5e615660f660e28b8def20cbfdc
tomoharu33 <tomoharu.n.33@gmail.com>
2021-02-04 19:23:37 +0900

    create errors partial

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: books
+#
+#  id         :bigint           not null, primary key
+#  body       :text
+#  title      :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  user_id    :integer
+#
 class Book < ApplicationRecord
   belongs_to :user
   has_one_attached :book_image
