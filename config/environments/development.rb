ee2a4831a918b5f5082bf5fa23528e8bb609266e
tomoharu33 <tomoharu.n.33@gmail.com>
2021-02-23 14:39:47 +0900

    modify devise login mail

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,6 +26,7 @@ Rails.application.configure do
 
   # for devise config
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  ActionMailer::Base.delivery_method = :letter_opener
 
   config.active_support.deprecation = :log
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: {
-    registrations: "users/registrations"
-  }
+  devise_for :users
   root to: "home#index"
   resources :users
   resources :books do
@@ -10,4 +8,6 @@ Rails.application.routes.draw do
 
   get 'search', to: "search#search"
   post 'search', to: "search#create"
+
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 end
