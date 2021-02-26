0a1e4b0921c73a265689eb619ee1c2ca5e38ad00
tomoharu33 <tomoharu.n.33@gmail.com>
2021-02-26 13:08:38 +0900

    modify Users to User

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,4 +1,4 @@
-class Users::RegistrationsController < Devise::RegistrationsController #deviseの該当クラスを継承させる
+class User::RegistrationsController < Devise::RegistrationsController #deviseの該当クラスを継承させる
   def create
     super do                                             # 他はdeviseの機能をそのまま流用する
       resource.update(confirmed_at: Time .now.utc)       # Welcomeメールを送信した上で、skip_confirmation!と同一処理を行い自動で認証クローズさせる
